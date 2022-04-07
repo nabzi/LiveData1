dcbc49b792ab4f72aa316b574fedb0d8cb477696
nabzi <zeinab.modir@gmail.com>
2022-04-07 14:00:31 +0430

    Add LiveDatas

--- app/src/main/java/com/example/livedata1/MainActivity.kt
+++ app/src/main/java/com/example/livedata1/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.livedata1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
     val vmodel : MainViewModel by viewModels()
@@ -13,6 +17,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        var textView = findViewById<TextView>(R.id.tvNumber)
+        var buttonNext = findViewById<Button>(R.id.button1)
+        var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        var questionText = findViewById<TextView>(R.id.tvQuestion)
+
+
+        progressBar.max = vmodel.questionCount
+
+        buttonNext.setOnClickListener {
+           vmodel.nextClicked()
+        }
+
+        val numberObserver = Observer<Int> { number ->
+                textView.text = number.toString()
+                progressBar.progress = number
+        }
+
+        val buttonEnabledObserver = Observer<Boolean>{  enabled ->
+            buttonNext.isEnabled = enabled
+        }
+
+        val questionObserver = Observer<String>{ question ->
+            questionText.text = question
+        }
+
+        vmodel.questionLiveData.observe(this , questionObserver)
+        vmodel.nextEnabledLiveData.observe(this , buttonEnabledObserver)
+        vmodel.numberLiveData.observe(this , numberObserver)
 
     }
 }
--- app/src/main/java/com/example/livedata1/MainViewModel.kt
+++ app/src/main/java/com/example/livedata1/MainViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.livedata1
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel  : ViewModel(){
 
-    var number = 0
+    //var number = 0
+    val numberLiveData = MutableLiveData<Int>(0)
 
+    val questionLiveData = MutableLiveData<String>(
+        QuestionRepository.questionList[0]
+    )
+
+    val questionCount = QuestionRepository.questionList.size
+
+    var nextEnabledLiveData = MutableLiveData<Boolean>(true)
+
+    fun nextClicked() {
+        numberLiveData.value = numberLiveData.value?.plus(1)
+        numberLiveData.value?.let{ number ->
+            questionLiveData.value = QuestionRepository.questionList[number]
+        }
+    }
 
 
 }
